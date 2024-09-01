e92153b67b097c9e36a12ea26126669e1e980e08
Deepak saroj <deepakkumarsaroj600@gmail.com>
2024-09-01 17:32:57 +0530

    App is live now

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -4,24 +4,23 @@ import generateTokenAndSetCookie from "../utils/generateTokens.js";
 
 export const signup = async (req, res) => {
   try {
-    const { fullName, username, password, confirmpassword, gender } = req.body;
+    const { fullName, username, password, confirmPassword, gender } = req.body;
 
-    if (password !== confirmpassword) {
-      return res.status(400).json({ error: "Password don't match " });
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords don't match" });
     }
 
     const user = await User.findOne({ username });
 
     if (user) {
-      return res.status(400).json({ error: "Username already exisats" });
+      return res.status(400).json({ error: "Username already exists" });
     }
 
-    //hash password
-
+    // HASH PASSWORD HERE
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    //https://avatar.iran.liara.run/public/boy?username=Scott
+    // https://avatar-placeholder.iran.liara.run/
 
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
     const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
@@ -35,9 +34,8 @@ export const signup = async (req, res) => {
     });
 
     if (newUser) {
-      //generate JWT
-      await generateTokenAndSetCokkie(newUser._id, res);
-
+      // Generate JWT token here
+      generateTokenAndSetCookie(newUser._id, res);
       await newUser.save();
 
       res.status(201).json({
@@ -51,7 +49,7 @@ export const signup = async (req, res) => {
     }
   } catch (error) {
     console.log("Error in signup controller", error.message);
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
@@ -68,7 +66,7 @@ export const login = async (req, res) => {
       return res.status(400).json({ error: "Invalid username or password" });
     }
 
-    generateTokenAndSetCokkie(user._id, res);
+    generateTokenAndSetCookie(user._id, res);
 
     res.status(200).json({
       _id: user._id,
@@ -78,7 +76,7 @@ export const login = async (req, res) => {
     });
   } catch (error) {
     console.log("Error in login controller", error.message);
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
@@ -88,6 +86,6 @@ export const logout = (req, res) => {
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
     console.log("Error in logout controller", error.message);
-    res.status(500).json({ error: "Internal server error1" });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
   try {
@@ -27,17 +28,23 @@ export const sendMessage = async (req, res) => {
       conversation.messages.push(newMessage._id);
     }
 
-    //socket io
-
     // await conversation.save();
     // await newMessage.save();
 
+    // this will run in parallel
     await Promise.all([conversation.save(), newMessage.save()]);
 
-    res.status(201).json({ message: "Message sent successfully" });
+    // SOCKET IO FUNCTIONALITY WILL GO HERE
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      // io.to(<socket_id>).emit() used to send events to specific client
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
+
+    res.status(201).json(newMessage);
   } catch (error) {
-    console.log("Error in sendmessage controller", error.message);
-    res.status(500).json({ error: "Internal server error2" });
+    console.log("Error in sendMessage controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
   }
 };
 
@@ -56,7 +63,7 @@ export const getMessages = async (req, res) => {
 
     res.status(200).json(messages);
   } catch (error) {
-    console.log("Error in getmessage controller", error.message);
-    res.status(500).json({ error: "Internal server error4" });
+    console.log("Error in getMessages controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
   }
 };
